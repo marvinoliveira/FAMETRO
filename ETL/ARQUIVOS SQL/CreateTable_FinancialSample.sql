9616a3684f2f287acd85fe41f1b719660b01fbbe
marvinoliveira <marvin.deoliveira@gmail.com>
2019-02-02 15:55:17 -0300

    Geração do modelo OLTP

---
+++ ETL/ARQUIVOS SQL/CreateTable_FinancialSample.sql
@@ -0,0 +1,20 @@
+CREATE TABLE FinancialSample
+(
+  Segment VARCHAR(100)
+, Country VARCHAR(100)
+, Product VARCHAR(100)
+, "Discount Band" VARCHAR(100)
+, "Units Sold" FLOAT(53)
+, "Manufacturing Price" FLOAT(53)
+, "Sale Price" FLOAT(53)
+, "Gross Sales" FLOAT(53)
+, Discounts FLOAT(53)
+, " Sales" FLOAT(53)
+, COGS FLOAT(53)
+, Profit FLOAT(53)
+, "Date" DATETIME
+, "Month Number" FLOAT(53)
+, "Month Name" VARCHAR(100)
+, "Year" VARCHAR(100)
+)
+;
